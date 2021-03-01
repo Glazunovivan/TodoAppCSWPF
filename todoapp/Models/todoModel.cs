2086a4018bfe9e0f139873ada096040867c227ac
Ivan <ivan.glaz.2029@mail.ru>
2021-03-01 15:51:18 +0300

    Add project
    Finished project

---
+++ todoapp/Models/todoModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todoapp.Models
+{
+    class todoModel: INotifyPropertyChanged
+    {
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+        public bool isDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("isDone");
+            }
+        }
+
+        private string _text;
+
+        public string text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+            
+        }
+    }
+}
---
+++ todoapp/Services/FileIOSets.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using todoapp.Models;
+
+namespace todoapp.Services
+{
+    class FileIOSets
+    {
+        private readonly string PATH;
+        
+        public FileIOSets(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<todoModel> LoadData()
+        {
+            //проверяем существует ли файл или нет
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<todoModel>();
+            }
+            using(var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<todoModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object _todoData)
+        {
+            using(StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(_todoData);
+                writer.Write(output);
+            }
+            
+        }
+    }
+}
